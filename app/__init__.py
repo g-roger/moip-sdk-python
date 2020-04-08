03cc58d340a6ed00d9d5180cb3422badd3d0b488
Gabriel Roger <gabriel.nroger@gmail.com>
2020-04-08 15:44:43 -0300

    Add configuration of application to init

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+MOIP_API_KEY = os.getenv('MOIP_API_KEY')
+MOIP_API_URL = os.getenv('MOIP_API_URL', 'https://sandbox.moip.com.br/v2/')
+
+if not MOIP_API_KEY:
+    raise Exception('Must set environment variable MOIP_API_KEY')
