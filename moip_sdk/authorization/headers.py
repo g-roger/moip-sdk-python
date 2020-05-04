8cddcfd7eaa5132a8dbaba48b3264e19f4f3f805
Gabriel Roger <gabriel.nroger@gmail.com>
2020-05-04 17:29:20 -0300

    Modify encoded authorization

--- moip_sdk/authorization/headers.py
+++ moip_sdk/authorization/headers.py
@@ -1,16 +1,11 @@
 import base64
-
 from moip_sdk import MOIP_API_KEY
 
-key = MOIP_API_KEY.encode('utf-8')
-authorization = base64.standard_b64decode(key)
-authorization = authorization.decode('utf-8')
+encoded_authorization = base64.b64encode(MOIP_API_KEY.encode('utf-8'))
+authorization = str(encoded_authorization, 'utf-8')
 
 
-def headers():
-    result = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + str(authorization)
-    }
-
-    return result
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + str(authorization)
+}
