d041a54f185defc293f9168f17b3177de4b5049d
Gabriel Roger <gabriel.nroger@gmail.com>
2020-05-12 15:32:32 -0300

    Validate moip api key on headers

--- moip_sdk/__init__.py
+++ moip_sdk/__init__.py
@@ -1,7 +1,4 @@
 import os
 
-MOIP_API_KEY = os.getenv('MOIP_API_KEY')
+MOIP_API_KEY = os.getenv('MOIP_API_KEY') or None
 MOIP_API_URL = os.getenv('MOIP_API_URL', 'https://sandbox.moip.com.br/v2')
-
-if not MOIP_API_KEY:
-    raise Exception('Must set environment variable MOIP_API_KEY')
--- moip_sdk/authorization/headers.py
+++ moip_sdk/authorization/headers.py
@@ -1,11 +1,14 @@
 import base64
 from moip_sdk import MOIP_API_KEY
 
-encoded_authorization = base64.b64encode(MOIP_API_KEY.encode('utf-8'))
-authorization = str(encoded_authorization, 'utf-8')
 
+headers = None
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Basic ' + str(authorization)
-}
+if MOIP_API_KEY is not None:
+    encoded_authorization = base64.b64encode(MOIP_API_KEY.encode('utf-8'))
+    authorization = str(encoded_authorization, 'utf-8')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + str(authorization)
+    }
